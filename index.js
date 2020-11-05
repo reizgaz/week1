33075e9817484768034c6881e5eadeee3a409d00
reizgaz <59108114+reizgaz@users.noreply.github.com>
2020-11-05 14:58:48 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+ require('http')
+.Server((req, res) => {
+  
+/*  const CORS = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET,POST,DELETE',
+    'Access-Control-Allow-Headers': 'Content-Type, Access-Control-Allow-Headers'
+};*/
+  res.writeHead(200, CORS);
+  if (req.url === '/login') return res.end('reizgaz');
+  if (req.url === '/v8') return res.end(process.versions.v8);
+  if (req.url === '/package.json') return require('fs').createReadStream('./package.json').pipe(res);
+  
+  res.end('reizgaz');
+})
+.listen(process.env.PORT);
