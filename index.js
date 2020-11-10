69d5cc0c5d54c0666abec9e0cd7ebb8aa9ecdeea
reizgaz <59108114+reizgaz@users.noreply.github.com>
2020-11-10 21:12:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
  require('http')
 .Server((req, res) => {
   
-/*  const CORS = {
+ const CORS = {
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Methods': 'GET,POST,DELETE',
     'Access-Control-Allow-Headers': 'Content-Type, Access-Control-Allow-Headers'
-};*/
+};
   res.writeHead(200, CORS);
   if (req.url === '/login') return res.end('reizgaz');
   if (req.url === '/v8') return res.end(process.versions.v8);
